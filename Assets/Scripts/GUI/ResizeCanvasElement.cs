f20be413fba2810d53c1945179ddc9c9e14d7890
Keyboard Destroyer <43577127+kbrddestroyer@users.noreply.github.com>
2024-05-16 21:58:48 +0300

    Gui changes

---
+++ Assets/Scripts/GUI/ResizeCanvasElement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResizeCanvasElement : MonoBehaviour
+{
+    [SerializeField] private RectTransform self;
+    [SerializeField] private Vector2 initialResolution;
+
+    private void OnRectTransformDimensionsChange()
+    {
+        float resize = self.rect.width / initialResolution.x * initialResolution.y; 
+        self.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, resize);
+
+        Debug.Log("Call");
+    }
+}
